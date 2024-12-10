aeeb87b5204268e2a8d99f2d8ed6adff5c4f6d5f
Xavier Marquez <marquez.x@gmail.com>
2024-12-10 23:39:44 +1300

    Full refactor to simplify, make more flexible, and avoid multiple checking of arguments.

--- R/api.R
+++ R/api.R
@@ -7,12 +7,15 @@
 #' @param prompts A list of prompts to send to the API. The class of this object
 #'   should match one of the supported APIs: "groq", "claude" (for the Anthropic
 #'   API), "openai", "gemini", or "llamafile" (for local
-#'   [llamafiles](https://github.com/mozilla-ocho/llamafile/)).
-#' @param model A string specifying the model to use.
+#'   [llamafiles](https://github.com/mozilla-ocho/llamafile/)). It will
+#'   typically be the result of a call to [prompt_list()].
+#' @param model A string specifying the model to use. Get available models with
+#'   [get_available_models()].
 #' @param prompt_name An optional string specifying the type of prompt.
 #' @param ... Additional arguments passed to specific `call_api` methods, such
 #'   as `max_retries`, `temperature`, `max_tokens`, `json_mode`, `system`,
-#'   `pause_cap`, `llamafile_path`, or `log`.
+#'   `pause_cap`, `llamafile_path`, or `log`. See the documentation for the
+#'   [openai_single_request()] and [llamafile_single_request()] for more.
 #'
 #' @return A tibble containing the API responses and usage information.
 #' @details This function is implemented as a generic with methods for different
@@ -27,8 +30,10 @@
 #'   Each method handles API-specific details such as endpoint URLs,
 #'   authentication, and response parsing.
 #'
-#' @seealso [build_prompts_from_files()] for creating prompts to use with this
+#' @seealso [prompt_list()] for creating prompts to use with this
 #'   function.
+#' @family generic
+#' @family multiple requests
 #' @export
 call_api <- function(prompts, model, prompt_name, ...) {
   UseMethod("call_api", prompts)
@@ -41,27 +46,37 @@ call_api.default <- function(prompts,
                              prompt_name,
                              ...) {
   # Undefined vars
-  id <- api <- NULL
+  id <- NULL
 
-  # Extract additional arguments from ...
+  # Extract default arguments from ...
   args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  max_tokens <- args$max_tokens %||% 300
   json_mode <- args$json_mode %||% FALSE
-  system <- args$system
-  pause_cap <- args$pause_cap %||% 1200
-  single_request_fun <- args$single_request_fun
-  response_validation_fun <- args$response_validation_fun
-  content_extraction_fun <- args$content_extraction_fun
-  llamafile_path <- args$llamafile_path
   log <- args$log %||% TRUE
+
+  # Validate log - must be a logical scalar
+  checkmate::assert_logical(log, len = 1)
+
+  # Validate json_mode - must be a logical scalar
+  checkmate::assert_logical(json_mode, len = 1)
+
+  if (missing(prompt_name)) {
+    prompt_name <- if (json_mode) "json" else "default"
+  }
+
+  # Validate arguments, including those in ...
+  validate_single_request(prompts, model, prompt_name, ...)
+
+  # Resolve functions using the helper
+  api <- class(prompts)[1]
+  resolved_functions <- resolve_functions(api, prompt_name)
+
+  single_request_fun <- resolved_functions$single_request_fun
+  content_extraction_fun <- resolved_functions$content_extraction_fun
 
   # Ensure prompt IDs are defined
   ids <- names(prompts)
   if (is.null(ids)) {
-    stop("Each prompt must have a unique name. Please name your prompts.")
+    cli::cli_abort("Each prompt must have a unique name. Please name your prompts.")
   }
 
   # Initialize response collection
@@ -69,12 +84,6 @@ call_api.default <- function(prompts,
 
   # Generate a unique hash for the prompt set
   prompt_set <- digest::digest(ids)
-
-  # Start Llamafile if applicable and not already running
-  if (!is.null(llamafile_path) && !is_llamafile_running()) {
-    message("Starting LLamafile: ", llamafile_path)
-    start_llamafile(llamafile_path)
-  }
 
   # Loop over prompts and process each
   for (i in seq_along(prompts)) {
@@ -91,16 +100,8 @@ call_api.default <- function(prompts,
     response <- do.call(single_request_fun, list(
       prompt = prompts[[i]],
       model = model,
-      n_candidates = n_candidates,
-      max_retries = max_retries,
-      temperature = temperature,
-      max_tokens = max_tokens,
-      json_mode = json_mode,
-      system = system,
-      response_validation_fun = response_validation_fun,
       content_extraction_fun = content_extraction_fun,
-      pause_cap = pause_cap,
-      quiet = !log
+      ...
     ))
 
     # Add prompt ID to the response
@@ -122,332 +123,110 @@ call_api.groq <- function(prompts,
                           model,
                           prompt_name,
                           ...) {
-  # Extract additional arguments from ...
-  args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  max_tokens <- args$max_tokens %||% 300
-  json_mode <- args$json_mode %||% TRUE
-  system <- args$system
-  pause_cap <- args$pause_cap %||% 1200
-  log <- args$log %||% TRUE
 
-  # Set defaults for model and prompt_name if missing
+  # Set defaults for model if missing
   if (missing(model)) {
     model <- get_default_model("groq")
   }
-
-  if (missing(prompt_name)) {
-    prompt_name <- if (json_mode) "json" else "default"
-  }
-
-  # Validate the arguments (optional, depending on your implementation)
-  validate_args_call_api(prompts = prompts,
-                         model = model,
-                         prompt_name = prompt_name,
-                         n_candidates = n_candidates,
-                         max_retries = max_retries,
-                         temperature = temperature,
-                         max_tokens = max_tokens,
-                         json_mode = json_mode,
-                         system = system,
-                         pause_cap = pause_cap,
-                         log = log)
-
-  # Resolve functions using the helper
-  resolved_functions <- resolve_functions("groq", prompt_name)
 
   # Call the generic NextMethod to handle the request
   NextMethod(prompts,
              model = model,
              prompt_name = prompt_name,
-             n_candidates = n_candidates,
-             max_retries = max_retries,
-             temperature = temperature,
-             max_tokens = max_tokens,
-             json_mode = json_mode,
-             system = system,
-             response_validation_fun = resolved_functions$response_validation_fun,
-             content_extraction_fun = resolved_functions$content_extraction_fun,
-             single_request_fun = resolved_functions$single_request_fun,
-             pause_cap = pause_cap,
-             log = log)
+             ...)
 }
 
 
 #' @export
 call_api.claude <- function(prompts, model, prompt_name, ...) {
-  # Extract additional arguments from ...
-  args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  max_tokens <- args$max_tokens %||% 300
-  json_mode <- args$json_mode %||% TRUE
-  system <- args$system
-  pause_cap <- args$pause_cap %||% 1200
-  log <- args$log %||% TRUE
 
   if (missing(model)) {
     model <- get_default_model(class(prompts)[1])
   }
 
-  if (missing(prompt_name)) {
-    prompt_name <- if (json_mode) "json" else "default"
-  }
-
-  validate_args_call_api(prompts = prompts,
-                         model = model,
-                         prompt_name = prompt_name,
-                         n_candidates = n_candidates,
-                         max_retries = max_retries,
-                         temperature = temperature,
-                         max_tokens = max_tokens,
-                         json_mode = json_mode,
-                         system = system,
-                         pause_cap = pause_cap,
-                         log = log)
-
-  # Resolve functions using the helper
-  resolved_functions <- resolve_functions("claude", prompt_name)
-
   NextMethod(prompts,
              model = model,
-             prompt_name = prompt_name,
-             n_candidates = n_candidates,
-             max_retries = max_retries,
-             temperature = temperature,
-             max_tokens = max_tokens,
-             json_mode = json_mode,
-             system = system,
-             response_validation_fun = resolved_functions$response_validation_fun,
-             content_extraction_fun = resolved_functions$content_extraction_fun,
-             single_request_fun = resolved_functions$single_request_fun,
-             pause_cap = pause_cap,
-             log = log)
+             prompt_name,
+             ...)
 }
 
 #' @export
 call_api.mistral <- function(prompts, model, prompt_name, ...) {
-  # Extract additional arguments from ...
-  args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  top_p <- args$top_p %||% 1
-  max_tokens <- args$max_tokens %||% 300
-  stop <- args$stop
-  random_seed <- args$random_seed
-  presence_penalty <- args$presence_penalty %||% 0
-  frequency_penalty <- args$frequency_penalty %||% 0
-  safe_prompt <- args$safe_prompt %||% FALSE
-  pause_cap <- args$pause_cap %||% 1200
-  log <- args$log %||% TRUE
 
   if (missing(model)) {
-    model <- get_default_model(class(prompts)[1])
+    model <- get_default_model("mistral")
   }
-
-  if (missing(prompt_name)) {
-    prompt_name <- if (args$json_mode %||% TRUE) "json" else "default"
-  }
-
-  validate_args_call_api(prompts = prompts,
-                         model = model,
-                         prompt_name = prompt_name,
-                         n_candidates = n_candidates,
-                         max_retries = max_retries,
-                         temperature = temperature,
-                         max_tokens = max_tokens,
-                         json_mode = args$json_mode %||% TRUE,
-                         system = args$system,
-                         pause_cap = pause_cap,
-                         log = log)
-
-  # Resolve functions using the helper
-  resolved_functions <- resolve_functions("mistral", prompt_name)
 
   NextMethod(prompts,
              model = model,
-             prompt_name = prompt_name,
-             n_candidates = n_candidates,
-             max_retries = max_retries,
-             temperature = temperature,
-             top_p = top_p,
-             max_tokens = max_tokens,
-             stop = stop,
-             random_seed = random_seed,
-             presence_penalty = presence_penalty,
-             frequency_penalty = frequency_penalty,
-             safe_prompt = safe_prompt,
-             json_mode = args$json_mode %||% TRUE,
-             pause_cap = pause_cap,
-             response_validation_fun = resolved_functions$response_validation_fun,
-             content_extraction_fun = resolved_functions$content_extraction_fun,
-             single_request_fun = resolved_functions$single_request_fun,
-             log = log)
+             prompt_name,
+             ...)
 }
 
 
 #' @export
 call_api.openai <- function(prompts, model, prompt_name, ...) {
-  # Extract additional arguments from ...
-  args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  max_tokens <- args$max_tokens %||% 300
-  json_mode <- args$json_mode %||% TRUE
-  system <- args$system
-  pause_cap <- args$pause_cap %||% 1200
-  log <- args$log %||% TRUE
 
   if (missing(model)) {
-    model <- get_default_model(class(prompts)[1])
+    model <- get_default_model("openai")
   }
-
-  if (missing(prompt_name)) {
-    prompt_name <- if (json_mode) "json" else "default"
-  }
-
-  validate_args_call_api(prompts = prompts,
-                         model = model,
-                         prompt_name = prompt_name,
-                         n_candidates = n_candidates,
-                         max_retries = max_retries,
-                         temperature = temperature,
-                         max_tokens = max_tokens,
-                         json_mode = json_mode,
-                         system = system,
-                         pause_cap = pause_cap,
-                         log = log)
-
-  # Resolve functions using the helper
-  resolved_functions <- resolve_functions("openai", prompt_name)
 
   NextMethod(prompts,
              model = model,
-             prompt_name = prompt_name,
-             n_candidates = n_candidates,
-             max_retries = max_retries,
-             temperature = temperature,
-             max_tokens = max_tokens,
-             json_mode = json_mode,
-             system = system,
-             response_validation_fun = resolved_functions$response_validation_fun,
-             content_extraction_fun = resolved_functions$content_extraction_fun,
-             single_request_fun = resolved_functions$single_request_fun,
-             pause_cap = pause_cap,
-             log = log)
+             prompt_name,
+             ...)
 }
 
 #' @export
 call_api.gemini <- function(prompts, model, prompt_name, ...) {
-  # Extract additional arguments from ...
-  args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  max_tokens <- args$max_tokens %||% 300
-  json_mode <- args$json_mode %||% TRUE
-  system <- args$system
-  pause_cap <- args$pause_cap %||% 1200
-  log <- args$log %||% TRUE
 
   if (missing(model)) {
-    model <- get_default_model(class(prompts)[1])
+    model <- get_default_model("gemini")
   }
-
-  if (missing(prompt_name)) {
-    prompt_name <- if (json_mode) "json" else "default"
-  }
-
-  validate_args_call_api(prompts = prompts,
-                         model = model,
-                         prompt_name = prompt_name,
-                         n_candidates = n_candidates,
-                         max_retries = max_retries,
-                         temperature = temperature,
-                         max_tokens = max_tokens,
-                         json_mode = json_mode,
-                         system = system,
-                         pause_cap = pause_cap,
-                         log = log)
-
-  # Resolve functions using the helper
-  resolved_functions <- resolve_functions("gemini", prompt_name)
 
   NextMethod(prompts,
              model = model,
-             prompt_name = prompt_name,
-             n_candidates = n_candidates,
-             max_retries = max_retries,
-             temperature = temperature,
-             max_tokens = max_tokens,
-             json_mode = json_mode,
-             system = system,
-             response_validation_fun = resolved_functions$response_validation_fun,
-             content_extraction_fun = resolved_functions$content_extraction_fun,
-             single_request_fun = resolved_functions$single_request_fun,
-             pause_cap = pause_cap,
-             log = log)
+             prompt_name,
+             ...)
 }
 
 
 #' @export
 call_api.llamafile <- function(prompts, model, prompt_name, ...) {
-  # Extract additional arguments from ...
   args <- list(...)
-  n_candidates <- args$n_candidates %||% 1
-  max_retries <- args$max_retries %||% 10
-  temperature <- args$temperature %||% 0.2
-  max_tokens <- args$max_tokens %||% 300
-  json_mode <- args$json_mode %||% TRUE
-  system <- args$system
-  pause_cap <- args$pause_cap %||% 1200
   llamafile_path <- args$llamafile_path
-  log <- args$log %||% TRUE
+  json_mode <- args$json_mode %||% FALSE
 
-  if (missing(model)) {
-    model <- "LLaMA_CPP"
+  if(is_llamafile_running()) {
+    running_model <- which_llamafile_running()
+    if(!missing(model) && model != running_model) {
+      cli::cli_abort(
+        "Running llamafile model ({running_model}) is not the model demanded ({model}). ",
+        "Kill the current instance of {running_model} with {.fn kill_llamafile()} before running {model}.")
+    }
+    model <- which_llamafile_running()
+  } else {
+    if(is.null(llamafile_path) && !missing(model)) {
+      llamafile_path <- fs::dir_ls(recurse = TRUE, regexp = paste0(model, ".+llamafile"))
+      if(length(llamafile_path == 0)) {
+        cli::cli_abort("No llamafile models found in this directory or in its subdirectory.")
+      }
+      if(length(llamafile_path > 1)) {
+        cli::cli_abort("Multiple llamafile models found in this directory matching {model}.")
+      }
+    }
+    cli::cli_alert_info("Starting LLamafile at {llamafile_path}.")
+    start_llamafile(llamafile_path)
+    model <- which_llamafile_running()
   }
 
   if (missing(prompt_name)) {
     prompt_name <- if (json_mode) "json" else "default"
   }
 
-  validate_args_call_api(prompts = prompts,
-                         model = model,
-                         prompt_name = prompt_name,
-                         n_candidates = n_candidates,
-                         max_retries = max_retries,
-                         temperature = temperature,
-                         max_tokens = max_tokens,
-                         json_mode = json_mode,
-                         system = system,
-                         pause_cap = pause_cap,
-                         log = log,
-                         llamafile_path = llamafile_path)
-
-  # Resolve functions using the helper
-  resolved_functions <- resolve_functions("llamafile", prompt_name)
-
   NextMethod(prompts,
              model = model,
-             prompt_name = prompt_name,
-             n_candidates = n_candidates,
-             max_retries = max_retries,
-             temperature = temperature,
-             max_tokens = max_tokens,
-             json_mode = json_mode,
-             system = system,
-             llamafile_path = llamafile_path,
-             response_validation_fun = resolved_functions$response_validation_fun,
-             content_extraction_fun = resolved_functions$content_extraction_fun,
-             single_request_fun = resolved_functions$single_request_fun,
-             pause_cap = pause_cap,
-             log = log)
+             ...)
 }
 
 
