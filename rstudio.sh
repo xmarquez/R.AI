1b9bf76f40f9a1d6bc64c7eef58f20d1b89ef69b
Xavier Marquez <marquez.x@gmail.com>
2024-12-13 11:36:59 +1300

    sbatch Rstudio script for working in the HPC cluster

---
+++ rstudio.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=rstudio
+#SBATCH --output=rstudio_%j.log
+#SBATCH --error=rstudio_%j.err
+#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=16G
+#SBATCH --partition=parallel
+
+# Load necessary modules
+module purge
+module load GCC/10.2.0 OpenMPI/4.0.5 Singularity/3.10.2
+
+# Start RStudio Server in the Singularity container with $PWD as the working directory
+PASSWORD='yourpassword' singularity exec \
+--home $PWD \
+--bind /nfs/scratch/marquexa/rstudio-tmp:/tmp \
+--bind /nfs/scratch/marquexa/rstudio-lib:/var/lib/rstudio-server \
+--bind /nfs/scratch/marquexa/rstudio-run:/var/run/rstudio-server \
+--bind ~/rsession.conf:/etc/rstudio/rsession.conf \
+/nfs/scratch/marquexa/Lyrics/lyrics_container.sif \
+rserver --auth-none=0 --auth-pam-helper-path=pam-helper --www-address=127.0.0.1 --server-user=$USER
+
+# Keep the job running so you can access RStudio Server via SSH tunnel
+while true; do sleep 60; done
